ec28b02b5f4e86343cec046e6c96b5e5c8bf2953
silmood <dsilmood@gmail.com>
2018-02-28 21:22:25 -0600

    User can send and receive messages

--- channels/assets/elm/src/Main.elm
+++ channels/assets/elm/src/Main.elm
@@ -3,7 +3,10 @@ module Main exposing (..)
 import Phoenix
 import Phoenix.Socket as Socket
 import Phoenix.Channel as Channel
-import Json.Decode as Json
+import Phoenix.Push as Push
+import Phoenix exposing (push)
+import Json.Encode as Encode
+import Json.Decode as Decode exposing (Decoder)
 import Html exposing (Html, Attribute, text, div, h1, img, h2, button, input, ul, li)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onSubmit, onClick)
@@ -33,6 +36,7 @@ type alias Model =
     { channel :  Maybe (Channel.Channel Msg)
     , stage: Stage
     , room : Room
+    , typedMsg : String
     }
 
 initModel : Model
@@ -40,6 +44,7 @@ initModel =
     { stage = Login
     , channel = Maybe.Nothing
     , room = { name = "", path = "", messages = [] }
+    , typedMsg = ""
     }
 
 init : ( Model, Cmd Msg )
@@ -57,13 +62,18 @@ socket =
 buildRoomChannel : String -> Channel.Channel Msg
 buildRoomChannel room =
     Channel.init room
+    |> Channel.withDebug
+    |> Channel.on "new_msg" NewMsg
     |> Channel.onJoin Online
 
 type Msg
     = RoomInput String
-    | Online Json.Value
+    | Online Encode.Value
     | Enter
     | Leave
+    | SendMsg
+    | TypeMsg String
+    | NewMsg Decode.Value
     | NoOp
 
 
@@ -76,19 +86,51 @@ update msg model =
                 updatedRoom =
                     { oldRoom | path = roomPath ++ roomName, name = roomName }
             in
-                ({ model | room = updatedRoom }, Cmd.none)
+                { model | room = updatedRoom } ! []
+
         Enter ->
             let
                 connectedChannel = buildRoomChannel model.room.path
             in
-                ({ model | channel = Just connectedChannel}, Cmd.none)
-        Online _ ->
-            ({ model | stage = Chat}, Cmd.none)
-        Leave ->
-            ({ model | stage = Login }, Cmd.none)
-        _ ->
-            ( model, Cmd.none )
+                { model | channel = Just connectedChannel} ! []
 
+        TypeMsg typed ->
+            { model | typedMsg = typed } ! []
+
+        SendMsg ->
+            { model | typedMsg = ""} ! [ sendMsg model ]
+
+        NewMsg payload ->
+            case Decode.decodeValue decodeNewMsg payload of
+                Ok msgReceived ->
+                    let
+                        oldRoom = 
+                            model.room
+
+                        messagesUpdated =
+                            List.append oldRoom.messages [msgReceived]
+
+                        roomUpdated =
+                            { oldRoom | messages = messagesUpdated}
+                    in
+                        ({ model | room = roomUpdated}, Cmd.none)
+                Err err ->
+                    model ! []
+
+        Online _ ->
+            { model | stage = Chat} ! []
+
+        Leave ->
+            { model | stage = Login } ! []
+
+        _ ->
+            model ! []
+
+-- Decoder
+
+decodeNewMsg : Decoder String
+decodeNewMsg =
+    Decode.field "msg" Decode.string
 
 ---- SUBS ----
 
@@ -104,13 +146,27 @@ connectChannel model =
         Sub.none
       Just channel ->
           Phoenix.connect socket [channel]
-          
+
+sendMsg : Model -> Cmd Msg
+sendMsg model =
+    let
+        payload =
+            Encode.object [("msg", Encode.string model.typedMsg)]
+
+        message =
+            Push.init model.room.path "new_msg"
+            |> Push.withPayload payload
+    in
+        push webSocketUrl message
+
+
 
 ---- VIEW ----
 
 view : Model -> Html Msg
 view model =
-    div [] [
+    div []
+        [
           case model.stage of
               Login ->
                 loginForm model
@@ -133,30 +189,31 @@ loginForm model =
 chatView : Model -> Html Msg
 chatView model =
     div 
-      [ -- Attributes
-      ] 
-      [ chatMessagesView model
-      , chatForm model
-      , button [ onClick Leave ] [ text "Leave" ]
-      ]
+        [] 
+        [ chatMessagesView model
+        , chatForm model
+        , button [ onClick Leave ] [ text "Leave" ]
+        ]
 
 chatMessagesView : Model -> Html Msg
 chatMessagesView model =
     div
-      [
-      ]
-      [ h2 [] [ text ("Welcome to " ++ model.room.name) ]
-      , ul [] (List.map messageView model.room.messages)
-      ]
+        []
+        [ h2 [] [ text ("Welcome to " ++ model.room.name) ]
+        , ul [] (List.map messageView model.room.messages)
+        ]
 
 chatForm : Model -> Html Msg
 chatForm model =
     Html.form
-      [
-      ]
-      [ input [ placeholder "Type message..." ] [] 
-      , button [] [ text "Send" ]
-      ]
+        [ onSubmit SendMsg ]
+        [ input 
+            [ placeholder "Type message..."
+            , value model.typedMsg
+            , onInput TypeMsg ] 
+            [] 
+        , button [] [ text "Send" ]
+        ]
 
 messageView : String -> Html Msg
 messageView message =
--- channels/lib/channels_web/channels/room_channel.ex
+++ channels/lib/channels_web/channels/room_channel.ex
@@ -7,5 +7,10 @@ defmodule Elmix.ChannelsWeb.RoomChannel do
     {:ok, socket}
   end
 
+  def handle_in("new_msg", %{"msg" => msg}, socket) do
+    Logger.info "Message: #{inspect(msg)}"
+    broadcast! socket, "new_msg", %{msg: msg}
+    {:reply, :ok, socket}
+  end
 
 end
